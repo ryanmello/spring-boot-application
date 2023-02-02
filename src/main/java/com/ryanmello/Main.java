40c5c4b1eda134d91c07e7ed1a62b16e25a6bab8
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-02 14:35:26 -0800

    java objects to json objects

--- src/main/java/com/ryanmello/Main.java
+++ src/main/java/com/ryanmello/Main.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -12,8 +15,21 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public String greet(){
-        return "Hello!";
+    @GetMapping("/")
+    public GreetResponse greet(){
+        GreetResponse response =  new GreetResponse(
+                "Hello",
+                List.of("Java", "Javascript"),
+                new Person("Alex", 28, 30_000)
+        );
+        return response;
     }
+
+    record Person(String name, int age, double savings){ }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){ }
 }
