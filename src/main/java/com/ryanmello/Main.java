ea739faff4addc63e927b1ae9a5138ec0ed7d571
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-03 16:39:55 -0800

    read customer api

--- src/main/java/com/ryanmello/Main.java
+++ src/main/java/com/ryanmello/Main.java
@@ -3,33 +3,28 @@ package com.ryanmello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-    public GreetResponse greet(){
-        GreetResponse response =  new GreetResponse(
-                "Hello",
-                List.of("Java", "Javascript"),
-                new Person("Alex", 28, 30_000)
-        );
-        return response;
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
-
-    record Person(String name, int age, double savings){ }
-
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-    ){ }
 }
